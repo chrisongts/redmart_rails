bcceed02bc8c3e94f7d9fd490fdd998d85a2a73c
Christopher Ong <chrisongts@gmail.com>
2016-07-27 15:31:02 +0800

    Login session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  # get '/signup', to: 'users#new'
+  get '/login', to: 'session#new'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
