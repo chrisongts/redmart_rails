a5c017d6859074c7b70d8310556fa72908bd9110
Christopher Ong <chrisongts@gmail.com>
2016-07-27 23:22:17 +0800

    changes in application and session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      flash[:success] = 'Success login'
+     user = User.find_by(email: params[:session][:email].downcase)
+    #  debugger
+    # if user is found
+    if user && user.authenticate( params[:session][:password] )
+      # not rendering
+      flash[:success] = 'Login successfully!'
+      log_in(user)
+      # session[:user_id] = user.id
       redirect_to user
-      # Log the user in and redirect to the user's show page.
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      # show error notice
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-
   end
 end
