f1d20272ab4150c66a458f4b42d59aa35a6c262e
Christopher Ong <chrisongts@gmail.com>
2016-07-28 13:35:08 +0800

    added static pages and modified home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  # get 'static_pages/about'
+  #
+  # get 'static_pages/help'
+
   root 'sessions#new'
   get 'sessions/new'
 
-  # get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   get '/login', to: 'session#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
